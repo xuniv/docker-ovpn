908ffc5ad636060b6b660de6b0ee71bfc9ef44d4
Malte Koch <36429230+baudcode@users.noreply.github.com>
2022-02-28 17:44:43 +0100

    Add compression

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,6 +67,7 @@ SAVED_DIR="${PWD}"
 cd $(dirname "${OPENVPN_CONFIG}")
 mkfifo /openvpn-fifo
 spawn openvpn \
+    --comp-lzo \
     --script-security 2 \
     --config "${OPENVPN_CONFIG}" \
     --up /usr/local/bin/openvpn-up.sh
